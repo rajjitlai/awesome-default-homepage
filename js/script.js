efabaed559ab68e1f1be4dea49423b3a9d440ba9
RAJJIT <2.0lairaj@gmail.com>
2023-09-10 13:55:57 +0530

    added-Category

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 const time = document.getElementById("time-view");
-
 setInterval(() => {
     const d = new Date();
     time.innerHTML = d.toLocaleTimeString();
@@ -9,25 +8,39 @@ const shortcutForm = document.getElementById("shortcut-form");
 const shortcutUrlInput = document.getElementById("shortcut-url");
 const shortcutNameInput = document.getElementById("shortcut-name");
 const shortcutList = document.getElementById("shortcut-list");
+const categoryName = document.getElementById("category-name");
+const categoryList = document.getElementById("category-list");
 
 const savedShortcuts = JSON.parse(localStorage.getItem("shortcuts")) || [];
 
-function displayShortcuts() {
+function extractCategories(shortcuts) {
+    const categories = [];
+    shortcuts.forEach(shortcut => {
+        if (!categories.includes(shortcut.category)) {
+            categories.push(shortcut.category);
+        }
+    });
+    return categories;
+}
+
+function displayShortcutsAndCategories() {
     shortcutList.innerHTML = "";
+    categoryList.innerHTML = "";
+
     savedShortcuts.forEach((shortcut, index) => {
         const shortcutElement = document.createElement("div");
         shortcutElement.className = "shortcut";
         shortcutElement.innerHTML = `
-                    <a href="${shortcut.url}" target="_blank">${shortcut.name}</a>
-                    <button class="remove-button" data-index="${index}">Remove</button>
-                `;
+                <a href="${shortcut.url}" target="_blank">${shortcut.name}</a>
+                <button class="remove-button" data-index="${index}">Remove</button>
+            `;
         shortcutList.appendChild(shortcutElement);
 
         const removeButton = shortcutElement.querySelector(".remove-button");
         removeButton.addEventListener("click", () => {
             savedShortcuts.splice(index, 1);
             localStorage.setItem("shortcuts", JSON.stringify(savedShortcuts));
-            displayShortcuts();
+            displayShortcutsAndCategories();
         });
 
         const anchorTag = shortcutElement.querySelector("a");
@@ -37,20 +50,30 @@ function displayShortcuts() {
                 removeButton.style.display = "none";
             }, 3000);
         });
+
+        if (!categoryList.textContent.includes(shortcut.category)) {
+            const categoryElement = document.createElement("div");
+            categoryElement.className = "category";
+            categoryElement.textContent = shortcut.category;
+            categoryList.appendChild(categoryElement);
+        }
     });
 }
-displayShortcuts();
+
+displayShortcutsAndCategories();
 
 shortcutForm.addEventListener("submit", function (e) {
     e.preventDefault();
 
     const url = shortcutUrlInput.value;
     const name = shortcutNameInput.value;
-    const shortcut = { url, name };
+    const category = categoryName.value;
+    const shortcut = { url, name, category };
     savedShortcuts.push(shortcut);
     localStorage.setItem("shortcuts", JSON.stringify(savedShortcuts));
-    displayShortcuts();
+    displayShortcutsAndCategories();
 
     shortcutUrlInput.value = "";
     shortcutNameInput.value = "";
+    categoryName.value = "";
 });
